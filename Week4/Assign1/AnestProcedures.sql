45de977593cfad32e3285dbad1c779dd3dbbceee
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-12 17:23:30 +0200

    create table AnestProcedures

--- Week4/Assign1/AnestProcedures.sql
+++ Week4/Assign1/AnestProcedures.sql
@@ -1,2 +1,8 @@
 CREATE DATABASE HospitalRecords;
 use HospitalRecords;
+CREATE TABLE IF NOT EXISTS AnestProcedures (
+    proc_id int NOT NULL UNIQUE AUTO_INCREMENT,
+    anest_name varchar(30) NOT NULL,
+    start_time time NOT NULL,
+    end_time time NOT NULL
+);
