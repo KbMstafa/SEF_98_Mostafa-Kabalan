e626feb39c2318bfcbbd0e19827b7db4ca055341
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-12 17:42:58 +0200

    fill table with records

--- Week4/Assign1/AnestProcedures.sql
+++ Week4/Assign1/AnestProcedures.sql
@@ -7,6 +7,15 @@ CREATE TABLE IF NOT EXISTS AnestProcedures (
     end_time time NOT NULL
 );
 
+INSERT INTO AnestProcedures VALUES (0, "'Albert'", "08:00", "11:00");
+INSERT INTO AnestProcedures VALUES (0, "'Albert'", "09:00", "13:00");
+INSERT INTO AnestProcedures VALUES (0, "'Kamal'", "08:00", "13:30");
+INSERT INTO AnestProcedures VALUES (0, "'Kamal'", "09:00", "15:30");
+INSERT INTO AnestProcedures VALUES (0, "'Kamal'", "10:00", "11:30");
+INSERT INTO AnestProcedures VALUES (0, "'Kamal'", "12:00", "13:30");
+INSERT INTO AnestProcedures VALUES (0, "'Kamal'", "13:30", "14:30");
+INSERT INTO AnestProcedures VALUES (0, "'Kamal'", "18:30", "19:00");
+
 DELIMITER @
 
 CREATE FUNCTION count_overlap (name varchar(30), curTime time)
@@ -16,18 +25,22 @@ CREATE FUNCTION count_overlap (name varchar(30), curTime time)
         SELECT COUNT(proc_id) INTO total
         FROM AnestProcedures
         WHERE ( name = IDanest_name )
-                AND /*( curTime BETWEEN start_time AND end_time)*/( ( curTime >= start_time ) AND ( curTime < end_time ) ) ;
+                AND ( ( curTime >= start_time ) AND ( curTime < end_time ) ) ;
         RETURN total;
     END; @
 
-CREATE FUNCTION count_overlap (curTime time, start_time time, end_time time)
+CREATE FUNCTION max_overlap (name varchar(30), startT time, endT time)
     RETURNS integer
     BEGIN
-        DECLARE total integer;
-        SELECT COUNT(proc_id) INTO total
-        FROM AnestProcedures
-        WHERE curTime BETWEEN start_time AND end_time;
-        RETURN total;
-    END;
+        DECLARE max integer;
+        DECLARE curTime time;
+        SET curTime = startT;
+        SET max = 0;
+        WHILE curTime <= endT DO
+            SET max = GREATEST(max, count_overlap (name, curTime));
+            SET curTime = ADDTIME(curTime,"00:05");
+        END WHILE;
+        RETURN max;
+    END; @
 
 DELIMITER ;
