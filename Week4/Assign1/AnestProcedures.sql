9390afb92674b47297a3d227db76954e78a4fcf3
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-12 17:25:57 +0200

    implement count_overlap function to count number of overlap in a specific time

--- Week4/Assign1/AnestProcedures.sql
+++ Week4/Assign1/AnestProcedures.sql
@@ -6,3 +6,18 @@ CREATE TABLE IF NOT EXISTS AnestProcedures (
     start_time time NOT NULL,
     end_time time NOT NULL
 );
+
+DELIMITER @
+
+CREATE FUNCTION count_overlap (name varchar(30), curTime time)
+    RETURNS integer
+    BEGIN
+        DECLARE total integer;
+        SELECT COUNT(proc_id) INTO total
+        FROM AnestProcedures
+        WHERE ( name = IDanest_name )
+                AND /*( curTime BETWEEN start_time AND end_time)*/( ( curTime >= start_time ) AND ( curTime < end_time ) ) ;
+        RETURN total;
+    END; @
+
+DELIMITER ;
