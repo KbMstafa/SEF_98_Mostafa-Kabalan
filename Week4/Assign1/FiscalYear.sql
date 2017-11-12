0af3dfa8f7623f7ab6ec01910762aec5710b27fc
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-12 20:46:37 +0200

    create table FiscalYearTable

--- Week4/Assign1/FiscalYear.sql
+++ Week4/Assign1/FiscalYear.sql
@@ -1,2 +1,7 @@
 CREATE DATABASE FinanceDB;
 use FinanceDB;
+CREATE TABLE IF NOT EXISTS FiscalYearTable (
+    fiscal_year int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    start_date date NOT NULL,
+    end_date date NOT NULL
+);
