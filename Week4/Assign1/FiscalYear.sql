d061632a39096f0caca5eb4f41723cc1df95e6ae
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-12 20:48:21 +0200

    create FY_start trigger to make sure the user insert right start_date

--- Week4/Assign1/FiscalYear.sql
+++ Week4/Assign1/FiscalYear.sql
@@ -5,3 +5,27 @@ CREATE TABLE IF NOT EXISTS FiscalYearTable (
     start_date date NOT NULL,
     end_date date NOT NULL
 );
+
+delimiter @
+
+CREATE TRIGGER FY_start
+    BEFORE INSERT
+    ON FiscalYearTable FOR EACH ROW
+    BEGIN
+        IF ( YEAR ( NEW.start_date ) = NEW.fiscal_year ) THEN
+            IF ( MONTH ( NEW.start_date ) = 10 ) THEN
+                IF ( DAY ( NEW.start_date ) <> 1 ) THEN
+                    SIGNAL SQLSTATE '45000'
+                        SET MESSAGE_TEXT = 'start_date day must be 01 (first)';
+                END IF;
+            ELSE
+                SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'start_date month must be 10 (october)';
+            END IF;
+        ELSE
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'start_date year must be equal to fiscal_year';
+        END IF;
+    END;@
+
+delimiter ;
