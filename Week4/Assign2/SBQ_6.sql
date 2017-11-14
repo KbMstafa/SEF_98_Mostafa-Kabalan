1749a029d12272229ddb931f13844e4d2a8aa9e0
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-14 22:38:51 +0200

    SBQ_6 fix bugs

--- Week4/Assign2/SBQ_6.sql
+++ Week4/Assign2/SBQ_6.sql
@@ -3,7 +3,7 @@ FROM category AS cat
 RIGHT JOIN (SELECT category_id, count(film_id) AS count 
             FROM film_category 
             GROUP BY category_id 
-            HAVING count(film_id) BETWEEN 40 AND 50 
+            HAVING count(film_id) BETWEEN 55 AND 65 
             ORDER BY count(film_id)) 55cat65
 ON cat.category_id = 55cat65.category_id
 UNION
@@ -13,7 +13,7 @@ RIGHT JOIN (SELECT category_id, if((SELECT count(*)
                                     FROM (SELECT count(film_id) AS count 
                                           FROM film_category 
                                           GROUP BY category_id 
-                                          HAVING count(film_id) BETWEEN 40 AND 50 ) 55cat65) = 0, 
+                                          HAVING count(film_id) BETWEEN 55 AND 65 ) 55cat65) = 0, 
                                    (SELECT max(count) 
                                     FROM (SELECT count(film_id) AS count 
                                           FROM film_category 
