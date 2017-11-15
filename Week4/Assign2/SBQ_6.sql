1f82526eaa7b54cebff8693c94990a981abc7b85
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-15 10:20:51 +0200

    fix coding style

--- Week4/Assign2/SBQ_6.sql
+++ Week4/Assign2/SBQ_6.sql
@@ -1,23 +1,24 @@
 SELECT name, count 
 FROM category AS cat 
-RIGHT JOIN 
-((SELECT category_id, count(film_id) AS count 
-            FROM film_category 
-            GROUP BY category_id 
-            HAVING count(film_id) BETWEEN 55 AND 65 
-            ORDER BY count(film_id))
-UNION
-(SELECT category_id, if((SELECT count(*) 
-                                    FROM (SELECT count(film_id) AS count 
-                                          FROM film_category 
-                                          GROUP BY category_id 
-                                          HAVING count(film_id) BETWEEN 55 AND 65 ) 55cat65) = 0, 
-                                   (SELECT max(count) 
-                                    FROM (SELECT count(film_id) AS count 
-                                          FROM film_category 
-                                          GROUP BY category_id) not55cat65),              
+RIGHT JOIN ((SELECT category_id, count(film_id) AS count 
+             FROM film_category 
+             GROUP BY category_id 
+             HAVING count(film_id) BETWEEN 55 AND 65 
+             ORDER BY count(film_id))
+        UNION
+            (SELECT category_id, if((SELECT count(*) 
+                                     FROM (SELECT count(film_id) AS count 
+                                           FROM film_category 
+                                           GROUP BY category_id 
+                                           HAVING count(film_id) 
+                                           BETWEEN 55 AND 65 ) 55cat65) = 0, 
+                                    (SELECT max(count) 
+                                     FROM (SELECT count(film_id) AS count 
+                                           FROM film_category 
+                                           GROUP BY category_id) not55cat65),
                                 "NULL") count 
-            FROM film_category 
-            GROUP BY category_id 
-            HAVING count(film_id) = count)) categ
+             FROM film_category 
+             GROUP BY category_id 
+             HAVING count(film_id) = count)
+            ) categ
 ON cat.category_id = categ.category_id;
