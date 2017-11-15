795d0d76ae81c24a7dd271d600b5021fc941ad59
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-16 00:02:39 +0200

    fill all the tables with the data of the assignment

--- Week4/Assign3/medical_institution.sql
+++ Week4/Assign3/medical_institution.sql
@@ -7,10 +7,22 @@ CREATE TABLE IF NOT EXISTS Claims (
     patient_name VARCHAR(30) NOT NULL
 );
 
+INSERT INTO Claims VALUES (1, 'Bassem Dghaidi'), 
+                          (2, 'Omar Breidi'), 
+                          (3, 'Marwan Sawwan');
+
 CREATE TABLE IF NOT EXISTS Defendants (
     claim_id INT NOT NULL,
     defendant_name VARCHAR(30) NOT NULL
 );
+
+INSERT INTO Defendants VALUES (1, 'Jean Skaff'),
+                              (1, 'Elie Meouchi'),
+                              (1, 'Radwan Sameh'),
+                              (2, 'Joseph Eid'),
+                              (2, 'Paul Syoufi'),
+                              (2, 'Radwan Sameh'),
+                              (3, 'Issam Awwad');
 
 CREATE TABLE IF NOT EXISTS LegalEvents (
     claim_id INT NOT NULL,
@@ -19,8 +31,27 @@ CREATE TABLE IF NOT EXISTS LegalEvents (
     change_date DATE NOT NULL
 );
 
+INSERT INTO LegalEvents VALUES (1, 'Jean Skaff', 'AP', '2016-01-01'),
+                               (1, 'Jean Skaff', 'OR', '2016-02-02'),
+                               (1, 'Jean Skaff', 'SF', '2016-01-01'),
+                               (1, 'Jean Skaff', 'CL', '2016-01-01'),
+                               (1, 'Radwan Sameh', 'AP', '2016-01-01'),
+                               (1, 'Radwan Sameh', 'OR', '2016-02-02'),
+                               (1, 'Radwan Sameh', 'SF', '2016-01-01'),
+                               (1, 'Elie Meouchi', 'AP', '2016-01-01'),
+                               (1, 'Elie Meouchi', 'OR', '2016-02-02'),
+                               (2, 'Radwan Sameh', 'AP', '2016-01-01'),
+                               (2, 'Radwan Sameh', 'OR', '2016-01-01'),
+                               (2, 'Paul Syoufi', 'AP', '2016-01-01'),
+                               (3, 'Issam Awwad', 'AP', '2016-01-01');
+
 CREATE TABLE IF NOT EXISTS ClaimStatusCodes (
     claim_status VARCHAR(2) NOT NULL,
     claim_status_desc VARCHAR(30) NOT NULL,
     claim_seq INT NOT NULL UNIQUE
 );
+
+INSERT INTO ClaimStatusCodes VALUES ('AP', 'Awaiting review panel', 1),
+                                    ('OR', 'Panel opinion rendered', 2),
+                                    ('SF', 'Suit filed', 3),
+                                    ('CL', 'Closed', 4);
