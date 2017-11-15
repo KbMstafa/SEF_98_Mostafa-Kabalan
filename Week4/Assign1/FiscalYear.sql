4fe957eac6d40366e15001d3919d8328b4859b26
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-15 11:32:40 +0200

    modify fiscal year exerecise

--- Week4/Assign1/FiscalYear.sql
+++ Week4/Assign1/FiscalYear.sql
@@ -1,51 +1,51 @@
-CREATE DATABASE FinanceDB;
-use FinanceDB;
+delimiter @
+
+CREATE DATABASE IF NOT EXISTS FinanceDB@
+use FinanceDB@
 CREATE TABLE IF NOT EXISTS FiscalYearTable (
     fiscal_year int NOT NULL PRIMARY KEY,
     start_date date NOT NULL,
     end_date date NOT NULL
-);
+)@
 
-delimiter @
-
-CREATE TRIGGER FY_start
+CREATE TRIGGER FY_add
     BEFORE INSERT
     ON FiscalYearTable FOR EACH ROW
     BEGIN
-        IF ( YEAR ( NEW.start_date ) = NEW.fiscal_year - 1 ) THEN
-            IF ( MONTH ( NEW.start_date ) = 10 ) THEN
-                IF ( DAY ( NEW.start_date ) <> 1 ) THEN
-                    SIGNAL SQLSTATE '45000'
-                        SET MESSAGE_TEXT = 'start_date day must be 1 (first)';
-                END IF;
-            ELSE
-                SIGNAL SQLSTATE '45000'
-                    SET MESSAGE_TEXT = 'start_date month must be 10 (october)';
-            END IF;
-        ELSE
+        IF ( TIMESTAMPDIFF(MONTH, NEW.start_date, NEW.end_date) < 6 ) THEN
             SIGNAL SQLSTATE '45000'
-                SET MESSAGE_TEXT = 'start_date year must be equal to fiscal_year';
+                SET MESSAGE_TEXT = 'a fiscal year length should be at least 6 months';
+        ELSEIF ( TIMESTAMPDIFF(MONTH, NEW.start_date, NEW.end_date) > 18 ) THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'a fiscal year length should be at most 18 months';
+        ELSEIF ((SELECT count(*) FROM (SELECT start_date, end_date 
+                  FROM FiscalYearTable 
+                  WHERE NEW.start_date BETWEEN start_date AND end_date
+                     OR NEW.end_date BETWEEN start_date AND end_date) t) <> 0
+                ) THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'the given fiscal_year overlaps with another fiscal_year';
         END IF;
-    END;@
+    END; @
 
-CREATE TRIGGER FY_end
-    BEFORE INSERT
+CREATE TRIGGER FY_upd
+    BEFORE UPDATE
     ON FiscalYearTable FOR EACH ROW
     BEGIN
-        IF ( YEAR ( NEW.end_date ) = NEW.fiscal_year ) THEN
-            IF ( MONTH ( NEW.end_date ) = 9 ) THEN
-                IF ( DAY ( NEW.end_date ) <> 30 ) THEN
-                    SIGNAL SQLSTATE '45000'
-                        SET MESSAGE_TEXT = 'end_date day must be 30 (last)';
-                END IF;
-            ELSE
-                SIGNAL SQLSTATE '45000'
-                    SET MESSAGE_TEXT = 'end_date month must be 9 (september)';
-            END IF;
-        ELSE
+        IF ( TIMESTAMPDIFF(MONTH, NEW.start_date, NEW.end_date) < 6 ) THEN
             SIGNAL SQLSTATE '45000'
-                SET MESSAGE_TEXT = 'end_date year must be next year of fiscal_year';
+                SET MESSAGE_TEXT = 'a fiscal year length should be at least 6 months';
+        ELSEIF ( TIMESTAMPDIFF(MONTH, NEW.start_date, NEW.end_date) > 18 ) THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'a fiscal year length should be at most 18 months';
+        ELSEIF ((SELECT count(*) FROM (SELECT start_date, end_date 
+                  FROM FiscalYearTable 
+                  WHERE NEW.start_date BETWEEN start_date AND end_date
+                     OR NEW.end_date BETWEEN start_date AND end_date) t) <> 0
+                ) THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'the given fiscal_year overlaps with another fiscal_year';
         END IF;
-    END;@
+    END; @
 
 delimiter ;
