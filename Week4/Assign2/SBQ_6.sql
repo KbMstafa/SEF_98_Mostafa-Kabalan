7e55a41d0e6ffafffe4b5a2d87a1e69ca9cae99b
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-15 10:08:04 +0200

    reduce to join to one

--- Week4/Assign2/SBQ_6.sql
+++ Week4/Assign2/SBQ_6.sql
@@ -1,15 +1,13 @@
 SELECT name, count 
 FROM category AS cat 
-RIGHT JOIN (SELECT category_id, count(film_id) AS count 
+RIGHT JOIN 
+((SELECT category_id, count(film_id) AS count 
             FROM film_category 
             GROUP BY category_id 
             HAVING count(film_id) BETWEEN 55 AND 65 
-            ORDER BY count(film_id)) 55cat65
-ON cat.category_id = 55cat65.category_id
+            ORDER BY count(film_id))
 UNION
-SELECT name, count 
-FROM category AS cat
-RIGHT JOIN (SELECT category_id, if((SELECT count(*) 
+(SELECT category_id, if((SELECT count(*) 
                                     FROM (SELECT count(film_id) AS count 
                                           FROM film_category 
                                           GROUP BY category_id 
@@ -21,5 +19,5 @@ RIGHT JOIN (SELECT category_id, if((SELECT count(*)
                                 "NULL") count 
             FROM film_category 
             GROUP BY category_id 
-            HAVING count(film_id) = count) catmax
-ON cat.category_id = catmax.category_id;
+            HAVING count(film_id) = count)) categ
+ON cat.category_id = categ.category_id;
