7c18b8cda406c708bd1e2c847510d537cd1ba451
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-15 23:03:40 +0200

    create table Defendants

--- Week4/Assign3/medical_institution.sql
+++ Week4/Assign3/medical_institution.sql
@@ -1,7 +1,13 @@
 CREATE DATABASE IF NOT EXISTS medical_institution;
+
 USE medical_institution;
 
 CREATE TABLE IF NOT EXISTS Claims (
     claim_id INT NOT NULL PRIMARY KEY,
     patient_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Defendants (
+    claim_id INT NOT NULL,
+    defendant_name VARCHAR(30)
 );
