a088f8c4708a71a70ca07fad17f5867de2747c50
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-16 02:02:45 +0200

    finish all the sql queries

--- Week4/Assign3/medical_institution.sql
+++ Week4/Assign3/medical_institution.sql
@@ -33,15 +33,15 @@ CREATE TABLE IF NOT EXISTS LegalEvents (
 
 INSERT INTO LegalEvents VALUES (1, 'Jean Skaff', 'AP', '2016-01-01'),
                                (1, 'Jean Skaff', 'OR', '2016-02-02'),
-                               (1, 'Jean Skaff', 'SF', '2016-01-01'),
-                               (1, 'Jean Skaff', 'CL', '2016-01-01'),
+                               (1, 'Jean Skaff', 'SF', '2016-03-01'),
+                               (1, 'Jean Skaff', 'CL', '2016-04-01'),
                                (1, 'Radwan Sameh', 'AP', '2016-01-01'),
                                (1, 'Radwan Sameh', 'OR', '2016-02-02'),
-                               (1, 'Radwan Sameh', 'SF', '2016-01-01'),
+                               (1, 'Radwan Sameh', 'SF', '2016-03-01'),
                                (1, 'Elie Meouchi', 'AP', '2016-01-01'),
                                (1, 'Elie Meouchi', 'OR', '2016-02-02'),
                                (2, 'Radwan Sameh', 'AP', '2016-01-01'),
-                               (2, 'Radwan Sameh', 'OR', '2016-01-01'),
+                               (2, 'Radwan Sameh', 'OR', '2016-02-01'),
                                (2, 'Paul Syoufi', 'AP', '2016-01-01'),
                                (3, 'Issam Awwad', 'AP', '2016-01-01');
 
@@ -55,3 +55,29 @@ INSERT INTO ClaimStatusCodes VALUES ('AP', 'Awaiting review panel', 1),
                                     ('OR', 'Panel opinion rendered', 2),
                                     ('SF', 'Suit filed', 3),
                                     ('CL', 'Closed', 4);
+
+SELECT 
+    PCC.claim_id, PCC.patient_name, C2S.claim_status
+FROM
+    (SELECT 
+        LC.claim_id,
+            Claims.patient_name,
+            MIN(LC.last_claim) AS claim_code
+    FROM
+        (SELECT 
+        LE.claim_id,
+            LE.defendant_name,
+            MAX(S2C.claim_seq) AS last_claim
+    FROM
+        LegalEvents AS LE, ClaimStatusCodes AS S2C
+    WHERE
+        LE.claim_status = S2C.claim_status
+    GROUP BY LE.defendant_name , LE.claim_id
+    ORDER BY LE.claim_id) AS LC, Claims
+    WHERE
+        Claims.claim_id = LC.claim_id
+    GROUP BY LC.claim_id) AS PCC,
+    ClaimStatusCodes AS C2S
+WHERE
+    PCC.claim_code = C2S.claim_seq
+ORDER BY PCC.claim_id;
