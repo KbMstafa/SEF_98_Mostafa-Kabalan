ddd6bf7006be59bcca1d2a6d8ed20ea03dc0cc97
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-17 12:54:02 +0200

    Create new user with all privileges to access sakila

--- Week4/Assign4/Config.php
+++ Week4/Assign4/Config.php
@@ -1,6 +1,14 @@
 <?php
+$dbCon = new mysqli('localhost','root','889723417');
+$query = "CREATE USER IF NOT EXISTS 'sakilaRental'@'localhost' IDENTIFIED BY 'sakila123';";
+$dbCon->query($query);
+$query = "GRANT ALL PRIVILEGES ON sakila.* TO 'sakilaRental'@'localhost';";
+$dbCon->query($query);
+$query = "FLUSH PRIVILEGES;";
+$dbCon->query($query);
+$dbCon->Close();
 $host = "localhost";
-$usrName = "root";
-$usrPass = "";
+$usrName = "sakilaRental";
+$usrPass = "sakila123";
 $dbName = "sakila";
 ?>
