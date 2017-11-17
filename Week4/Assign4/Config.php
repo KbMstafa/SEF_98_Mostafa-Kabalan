afd7466d03ae5a43df13600a8c9a50c3602d7db4
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-17 14:53:22 +0200

    remove new user creation

--- Week4/Assign4/Config.php
+++ Week4/Assign4/Config.php
@@ -1,12 +1,4 @@
 <?php
-$dbCon = new mysqli('localhost','root','889723417');
-$query = "CREATE USER IF NOT EXISTS 'sakilaRental'@'localhost' IDENTIFIED BY 'sakila123';";
-$dbCon->query($query);
-$query = "GRANT ALL PRIVILEGES ON sakila.* TO 'sakilaRental'@'localhost';";
-$dbCon->query($query);
-$query = "FLUSH PRIVILEGES;";
-$dbCon->query($query);
-$dbCon->Close();
 $host = "localhost";
 $usrName = "sakilaRental";
 $usrPass = "sakila123";
