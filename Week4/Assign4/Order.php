0437a3f6987a68ea4a08757363684c57d1da3133
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-11-19 16:47:28 +0200

    use MyASQLWrap to select all films

--- Week4/Assign4/Order.php
+++ Week4/Assign4/Order.php
@@ -2,13 +2,8 @@
     <form name = "customer input" action = "OrderProcess.php" method = "post">
         <?php
         require_once "Config.php";
-        $dbCon = new mysqli($host, $usrName, $usrPass, $dbName);
-        if($dbCon->connect_error) {
-            die($dbCon->connect_error);
-        }
-        else {
-            echo "<H1>Welcome to sakila : \n</H1>";
-        }
+        require "MySQLWrap.php";
+        $con = new MySQLWrap();
         $query = "SELECT 
                       f.film_id, f.title, 
                       f.rental_duration, f.rental_rate
@@ -25,13 +20,12 @@
                               return_date IS NULL
                           ORDER BY inventory_id)
                   GROUP BY i.film_id;";
-        $result = $dbCon->prepare($query);
-        $result->execute();
-        $result-> bind_result($id, $title, $duration, $amount);
+        echo "<b>Choose a film to rent :</b><br>";
         echo "<select name = \"films\">";
-        while ($result->fetch()) {
-            echo "<option value='".$id."'>
-                      $title &emsp; \"$amount for $duration days\"
+        $films = $con->selectFilms($query);
+        foreach ($films as $filmInfo) {
+            echo "<option value='".$filmInfo[0]."'>
+                      $filmInfo[1]
                   </option>";
         }
         echo "</select></br></br>";
