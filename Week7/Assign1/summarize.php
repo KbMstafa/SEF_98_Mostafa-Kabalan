bf5b58e31220d5eb3686572bc3166cf674821915
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-12-07 11:12:52 +0200

    add php page summarize.php that send the text to aylien to summarize it and take it output

---
+++ Week7/Assign1/summarize.php
@@ -0,0 +1,12 @@
+<?php
+require_once"src/vendor/autoload.php";
+
+$title = $_POST["title"];
+$text = $_POST["text"];
+$sentences_number = $_POST["sentences_number"];
+$textapi = new AYLIEN\TextAPI("902d185f", "030d0697982d6fe0eb11914fa314e97e");
+$summary = $textapi->Summarize(array('title' => $title, 'text' => $text, 'sentences_number' => $sentences_number));
+foreach ($summary->sentences as $sentence) {
+  echo $sentence,"</br></br>";
+}
+?>
