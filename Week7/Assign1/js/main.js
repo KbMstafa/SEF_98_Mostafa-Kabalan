49f0cad4046e677c6f170bb4983fdad5a96bd707
Mostafa Kabalan <mostafa.kabalan17@gmail.com>
2017-12-07 11:04:20 +0200

    add function that extract text from html code

--- Week7/Assign1/js/main.js
+++ Week7/Assign1/js/main.js
@@ -1,5 +1,7 @@
 var summarize = {
-    
+    HPregex: /(<h(2|3) (.*?)>(.*?)<\/h(2|3)>|<p (.*?)>(.*?)<\/p>|<li (.*?)>(.*?)<\/li>)/g,
+    Hregex: /<h1 (.*?)>(.*?)<\/h1>/,
+    removeTagsRegex: /(<(.*?)>|<\/(.*?))>/g,
     init: function() {
         var getButton = document.getElementById("get");
         get.addEventListener("click", this.getContents);
@@ -17,11 +19,25 @@ var summarize = {
         {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 var htmlCode = xmlhttp.responseText;
-                console.log(htmlCode);
+                summarize.extractContent(htmlCode);
             }
         }
         xmlhttp.open("GET", "proxy.php?url=" + theUrl, true);
         xmlhttp.send(); 
+    },
+    extractContent: function(content)
+    {
+        var filtered = content.match(this.HPregex);
+
+        var span = document.createElement('span');
+
+        span.innerHTML = filtered;
+
+        var parsing = [span.textContent || span.innerText].toString();
+        var cleanupParsing = parsing.replace(/(\. ,)/g,". ");
+        var title = content.match(this.Hregex)[2];
+
+        summarize.summarization(cleanupParsing, title);
     }
 }
 
